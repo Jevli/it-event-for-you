efa96ec2600c63d99ddf3c5b748cd8d2b64dd1f4
Joni Laurila <joni.laurila@gofore.com>
2017-11-21 14:29:58 +0200

    Fixed auth middleware

--- web/routes/evensi-client.js
+++ web/routes/evensi-client.js
@@ -2,16 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var authToken;
-/*
-var authentication = (req, res, next) => {
-  if (!authToken) {
-    //Authentication token should be got here if missing
-  }
-  next()
-}
 
-router.use(authentication())
-*/
+router.use( (req, res, next) => {
+  if (!authToken) {
+
+  }
+
+  next()
+})
 
 /* Will get all future events from evensi */
 router.get('/getAllFutureEvents', function(req, res, next) {
