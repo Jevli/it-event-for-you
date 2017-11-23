b5fbf2aabd4a761a112769a1e662aa2f7f645e66
Joni Laurila <joni.laurila@hotmail.com>
2017-11-23 18:34:53 +0200

    Added authentication for evensiClient

--- web/routes/evensi-client.js
+++ web/routes/evensi-client.js
@@ -1,20 +1,52 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var https = require('https')
+var router = express.Router()
+var evensi = require('../config.json').evensi
 
-var authToken;
+var authToken
 
-router.use( (req, res, next) => {
-  if (!authToken) {
-
+const authenticate = async (req, res, next) => {
+  if(!authToken) {
+    try {
+      authToken = await getAuthToken(
+        {
+          host: evensi.evensi_path,
+          path: evensi.api_version + '/authenticate/app?appid='+ evensi.app_id +'&appsecret=' + evensi.app_secret
+        }
+      )
+    } catch(e) {
+      console.log(e)
+    }
   }
-
   next()
-})
+}
+
+const getAuthToken = (url) => {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let body = ''
+
+      res.on('data', (d) => {
+        body += d;
+      })
+
+      res.on('end', (d) => {
+        resolve(JSON.parse(body).data[0].token)
+      })
+    }).on('error', (e) => {
+      reject(e)
+    })
+  })
+}
+
+router.use(authenticate)
 
 /* Will get all future events from evensi */
 router.get('/getAllFutureEvents', function(req, res, next) {
   // Go throught all days
   // save them to database
-});
+  console.log("hello")
+  res.send("Hello")
+})
 
-module.exports = router;
+module.exports = router
