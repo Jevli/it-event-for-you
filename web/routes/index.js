e82b631816a2b9014f26cee7808362afb2bebfd3
Marja Ahonen <marja.e.ahonen@gmail.com>
2017-12-05 18:08:37 +0200

    removed add users method since it likely won't be used

--- web/routes/index.js
+++ web/routes/index.js
@@ -95,17 +95,4 @@ const searchKeywords = (name, desc) => {
   return keys
 }
 
-const addUsers = count => {
-  for (let i = 0; i < count; i++) {
-    let words = []
-    for (let j = 0; j < 10; j++) {
-      const random = Math.floor(Math.random() * keywords.length)
-      // unique??
-      words.push(keywords[random])
-    }
-    console.log("user: ", i);
-    console.log(words);
-  }
-}
-
 module.exports = router;
