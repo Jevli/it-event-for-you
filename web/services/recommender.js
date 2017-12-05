b0281773a9fff5a0e3f1b13fd0cc8e850d47869b
Marja Ahonen <marja.e.ahonen@gmail.com>
2017-12-05 18:09:39 +0200

    maybe shows results?

--- web/services/recommender.js
+++ web/services/recommender.js
@@ -3,20 +3,6 @@ const Database = require('./database-sevice'),
       keywords = require('../assets/keywords')
 
 class Recommender {
-  findRecommendable(userId) {
-    // user's keywords
-    const userKeywords = db.getUserKeywords(userId)
-    // past events user has been to
-    const pastEvents = db.getPastEvents(userId)
-    // TODO: loop the past events
-    // (tags and) keywords from those events
-    const eventMetaData = db.getEventMetaData(id)
-    // TODO: figure out how to select the most similar event?
-    // event's data
-    const eventInfo = db.getEventData(id)
-    return eventInfo
-  }
-
   // returns 3 events with largest keyword count
   findITEvent(days) {
     let events = db.getFutureEvents(days)
@@ -34,8 +20,6 @@ class Recommender {
     if (maxCounts.length > 2) {
       third = getKeywordCountEvents(events, maxCounts[2])
     }
-    // TODO: fetch event data if not fetched in earlier query
-    // const eventInfo = db.getEventData(id)
     return get3First(first, second, third)
   }
 
