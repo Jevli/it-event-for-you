d6f4f2244cd5d145122927f421cb72805b7ef937
Marja Ahonen <marja.e.ahonen@gmail.com>
2017-12-05 18:09:13 +0200

    removed user specific recommendation's method draft

--- web/routes/recommend.js
+++ web/routes/recommend.js
@@ -4,12 +4,10 @@ const express = require('express'),
       Recommender = require('../services/recommender'),
       recommender = new Recommender()
 
-router.get('/event/', (req, res, next) => {
-  const bestEvent = recommender.findRecommendable(req.query.user)
-})
-
 router.get('/itevent', (req, res, next) => {
   const days = req.query.days ? req.query.days : 10
   const itEvent = recommender.findITEvent(days)
   // TODO: display returned events
+  // ??????
+  res.json(itEvent)
 })
