2db481b4863893c6756618841169a020e743f579
Joni Laurila <joni.laurila@gofore.com>
2017-12-08 09:25:58 +0200

    adding initial file

--- web/models/event.js
+++ web/models/event.js
@@ -3,7 +3,14 @@ const db = require('../utils/dbConnection')
 const Events = {
   saveEvent: (event, cb) => db.create('events', event, cb),
   getAllEvents: (cb) => db.get('events', cb),
-  getEventById: (id, cb) => db.get('events', 'eventid', id, cb)
+  getEventById: (id, cb) => db.get('events', 'eventid', id, cb),
+  getFutureEvents: (day, cb) => {
+    getAllEvents( () => {
+      if (err) return (err)
+
+      
+    })
+  }
 }
 
 module.exports = Events
